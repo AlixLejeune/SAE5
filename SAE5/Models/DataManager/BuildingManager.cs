34754e2e65cb382c56ea2b9dbc7a1519884111b5
Ethan_L <ethan.legros@gmail.com>
2024-11-22 11:46:10 +0100

    managers done until funitureType

---
+++ SAE5/Models/DataManager/BuildingManager.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE501_Blazor_API.Models.EntityFramework;
+using SAE501_Blazor_API.Models.Repositories;
+
+namespace SAE501_Blazor_API.Models.DataManager
+{
+    public class BuildingManager : IDataRepository<Building>
+    {
+        readonly DataContext _context;
+
+        public BuildingManager() { }
+
+        public BuildingManager(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Building>>> GetAllAsync()
+        {
+            return await _context.buildings.ToListAsync();
+        }
+
+        public async Task<ActionResult<Building>> GetByIdAsync(int id)
+        {
+            return await _context.buildings.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task AddAsync(Building entity)
+        {
+            await _context.buildings.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Building originalBuilding, Building updatedBuilding)
+        {
+            _context.Entry(originalBuilding).State = EntityState.Modified;
+            originalBuilding.Name = updatedBuilding.Name;
+            originalBuilding.Letter = updatedBuilding.Letter;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Building buildingToDelete)
+        {
+            _context.buildings.Remove(buildingToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SAE5/Models/DataManager/DoorManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE501_Blazor_API.Models.EntityFramework;
+using SAE501_Blazor_API.Models.Repositories;
+
+namespace SAE501_Blazor_API.Models.DataManager
+{
+    public class DoorManager : IDataRepository<Door>
+    {
+        readonly DataContext _context;
+
+        public DoorManager() { }
+
+        public DoorManager(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Door>>> GetAllAsync()
+        {
+            return await _context.doors.ToListAsync();
+        }
+
+        public async Task<ActionResult<Door>> GetByIdAsync(int id)
+        {
+            return await _context.doors.FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task AddAsync(Door entity)
+        {
+            await _context.doors.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Door originalDoor, Door updatedDoor)
+        {
+            _context.Entry(originalDoor).State = EntityState.Modified;
+            originalDoor.Length = updatedDoor.Length;
+            originalDoor.Height = updatedDoor.Height;
+            originalDoor.WallId = updatedDoor.WallId;
+            originalDoor.Wall = updatedDoor.Wall;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Door doorToDelete)
+        {
+            _context.doors.Remove(doorToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
